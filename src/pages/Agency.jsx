30f067594227eaf6db639a1046a43c806c26f7c7
fahimnirjon <fahimnirjon21@gmail.com>
2026-01-29 20:48:00 +0600

    bugs fixed

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,89 +1,93 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import React, { useRef } from "react";
+import { useRef } from "react";
 
 const Agency = () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const imgDivRef = useRef(null);
-  const imgRef = useRef(null);
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
 
-  const imgArray = [
-    "/public/images/Carl_480x640.jpg",
-    "/public/images/CAMILLE_480X640_2.jpg",
-    "/public/images/ChantalG_480x640.jpg",
-    "/public/images/joel_480X640_3.jpg",
-    "/public/images/MEGGIE_480X640_2.jpg",
-    "/public/images/MEL_480X640.jpg",
-    "/public/images/Michele_480X640.jpg",
-    "/public/images/Olivier_480x640.jpg",
+  const imageArray = [
+    "https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg",
+    "https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg",
   ];
 
   useGSAP(function () {
-    gsap.to(imgDivRef.current, {
+    gsap.to(imageDivRef.current, {
       scrollTrigger: {
-        trigger: imgDivRef.current,
-        start: "top 30%",
+        trigger: imageDivRef.current,
+        // markers: true,
+        start: "top 28%",
         end: "top -70%",
         pin: true,
-        scrub: 1,
         pinSpacing: true,
         pinReparent: true,
         pinType: "transform",
+        scrub: 1, // smooth scrubbing with 1s easing
         anticipatePin: 1,
         invalidateOnRefresh: true,
         onUpdate: (elem) => {
-          let imgIndex;
-
+          let imageIndex;
           if (elem.progress < 1) {
-            imgIndex = Math.floor(elem.progress * imgArray.length);
+            imageIndex = Math.floor(elem.progress * imageArray.length);
           } else {
-            imgIndex = imgArray.length - 1;
+            imageIndex = imageArray.length - 1;
           }
-
-          imgRef.current.src = imgArray[imgIndex];
+          imageRef.current.src = imageArray[imageIndex];
         },
       },
     });
   });
 
   return (
-    <div>
-      <div className="section1 relative py-1">
+    <div className="parent">
+      <div id="page1" className="py-1 ">
         <div
-          ref={imgDivRef}
-          className="absolute overflow-hidden h-[20vw] w-[15vw] rounded-3xl top-0 left-[30vw]"
+          ref={imageDivRef}
+          className="absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]"
         >
           <img
-            ref={imgRef}
-            className="h-full w-full object-cover"
-            src="/public/images/Carl_480x640.jpg"
+            ref={imageRef}
+            className="h-full object-cover w-full"
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
             alt=""
           />
         </div>
-
         <div className="relative font-[font2]">
-          <div className="mt-[55vh]">
+          <div className="lg:mt-[55vh] mt-[30vh]">
             <h1 className="text-[20vw] text-center uppercase leading-[18vw]">
-              SEVEN7Y <br />
-              TWO
+              Soixan7e <br />
+              Douze
             </h1>
-            <div className="pl-[40%]">
-              <p className="text-6xl">
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                We’re inquisitive and open-minded, and we make sure creativity
-                crowds out ego from every corner. A brand is a living thing,
-                with values, a personality and a story. If we ignore that, we
-                can achieve short-term success, but not influence that goes the
-                distance. We bring that perspective to every brand story we help
-                tell.
-              </p>
-            </div>
+          </div>
+          <div className="lg:pl-[40%] lg:mt-20 mt-4 p-3">
+            <p className="lg:text-6xl text-xl leading-tight">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Notre curiosité nourrit notre créativité. On reste humbles et on
+              dit non aux gros egos, même le vôtre. Une marque est vivante. Elle
+              a des valeurs, une personnalité, une histoire. Si on oublie ça, on
+              peut faire de bons chiffres à court terme, mais on la tue à long
+              terme. C’est pour ça qu’on s’engage à donner de la perspective,
+              pour bâtir des marques influentes.
+            </p>
           </div>
         </div>
       </div>
-      <div className="section2 h-screen"></div>
+      <div id="page2" className=" h-screen"></div>
     </div>
   );
 };
