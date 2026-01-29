db2c1a85be0d29a74336086056e239c07c353262
fahimnirjon <fahimnirjon21@gmail.com>
2026-01-29 19:54:52 +0600

    bottom file changed a bit

--- src/components/home/HomeBottom.jsx
+++ src/components/home/HomeBottom.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const HomeBottom = () => {
 
@@ -11,7 +11,7 @@ const HomeBottom = () => {
         <Link className='text-[6vw] lg:mt-6' to='/projects'>Projects</Link>
       </div>
       <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50]  lg:h-44 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
-        <Link className='text-[6vw] lg:mt-6' to='/agence'>agence</Link>
+        <Link className='text-[6vw] lg:mt-6' to='/agency'>agency</Link>
       </div>
     </div>
   )
